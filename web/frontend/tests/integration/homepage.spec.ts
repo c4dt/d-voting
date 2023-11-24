876ae680985511549e70e7ac430fc52d40a0c317
Carine Dengler <git@carine-dengler.de>
2023-11-24 16:24:46 +0100

    test: added test that checks that cookie is set on login

--- web/frontend/tests/integration/homepage.spec.ts
+++ web/frontend/tests/integration/homepage.spec.ts
@@ -1,6 +1,27 @@
 import { test, expect } from '@playwright/test';
+import { default as i18n } from 'i18next';
+import en from './../../src/language/en.json';
+import de from './../../src/language/de.json';
+import fr from './../../src/language/fr.json';
+
+const resources = { de, en, fr };
+
+i18n.init({
+  resources,
+  fallbackLng: ['en', 'fr', 'de'],
+  debug: true,
+});
 
 test('Assert homepage title', async({ page }) => {
   await page.goto(process.env.FRONT_END_URL);
   await expect(page).toHaveTitle(/D-Voting/);
+});
+
+test('Assert login button', async({ page }) => {
+  await page.goto(process.env.FRONT_END_URL);
+  const login = page.getByRole('button', { name: i18n.t('login') });
+  await expect(login).toBeVisible();
+  await login.click();
+  const cookies = await page.context().cookies();
+  expect(cookies.find(cookie => cookie.name == 'connect.sid')).toBeTruthy();
 });
